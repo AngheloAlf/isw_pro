a1a7a9c245fd2b41844de156832feaa39e0010ea
Anghelo <angheloalf95@gmail.com>
2017-05-02 16:53:02 -0300

    Ver logs login esta un poco mas leible

--- project/public/angular/viewLogsLogin.js
+++ project/public/angular/viewLogsLogin.js
@@ -2,9 +2,31 @@
  * Created by Anghelo on 02-05-2017.
  */
 
+function timeToString(seconds){
+    var horas = parseInt(seconds/(60*60));
+    var minutos = parseInt(seconds/(60)) - horas*60;
+    var segundos = seconds - minutos * 60 - horas*60*60;
+    return horas+"h "+minutos+"m "+segundos+"s";
+}
+
 var app = angular.module('viewLogsLogin', []);
 app.controller('viewLogsLoginCtrl', function ($scope, $http){
-    $http.get("/logsLoginCrud/read").then(function (response) {
-        $scope.logs_login = response.data;
+    $http.get("/logsLoginCrud/read").then(function (response){
+        var logs_login = response.data;
+        logs_login.forEach(function(value){
+            var stripado = value.ip.split(":");
+            value.ip = stripado[stripado.length - 1];
+
+            var conn = value.conn_successful;
+            if(conn === 1){
+                value.conn_successful = "Si";
+            }
+            else{
+                value.conn_successful = "No";
+            }
+
+            value.timeOnline = timeToString(value.timeOnline);
+        });
+        $scope.logs_login = logs_login;
     });
 });
